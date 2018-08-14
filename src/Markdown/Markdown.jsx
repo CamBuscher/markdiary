b09278e55e0cbabd85514e9d728ba083f3ec323c
Cameron Buscher <cameronbuscher@gmail.com>
2018-08-14 14:29:26 -0600

    Messed with solution to add tabbing for markdown, will add later

--- src/Markdown/Markdown.jsx
+++ src/Markdown/Markdown.jsx
@@ -17,13 +17,18 @@ export class Markdown extends Component {
     this.setState({ [name]: value });
   }
 
+  
+
   render() {
     const converter = new showdown.Converter();
 
     return (
       <div>
         <textarea className='textarea' name="textarea" cols="30" rows="10" onChange={(e) => this.handleChange(e)}></textarea>
-        <div className='markdown_output' dangerouslySetInnerHTML={{ __html: converter.makeHtml(this.state.textarea) }}></div>
+        <div 
+          className='markdown_output' 
+          dangerouslySetInnerHTML={{ __html: converter.makeHtml(this.state.textarea) }}
+          ></div>
       </div>
     )
   }
