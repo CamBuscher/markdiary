965a1a781adc7f34b3092bf0b0b78fe72c5c4dfb
Cameron Buscher <cameronbuscher@gmail.com>
2018-09-16 22:02:59 -0600

    Add knex, set up initial migration
    First time using .uuid, seems exciting, but difficult to test properly.

---
+++ db/migrations/20180916215236_initial.js
@@ -0,0 +1,28 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('users', table => {
+      table.uuid('id').notNullablue().primary();
+      table.string('username').unique();
+      table.string('password');
+      table.timestamps(true, true)
+    }),
+
+    knex.schema.createTable('files', table => {
+      table.increments('id').primary();
+      table.string('title');
+      table.string('markdown');
+      table.uuid('user_id').unsigned()
+      table.foreign('user_id')
+        .references('users.id')
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.dropTable('files'),
+    knex.schema.dropTable('users')
+  ])
+};
