5e293cd6d58b6095ccbec087caf7558b311fc32a
Cameron Buscher <cameronbuscher@gmail.com>
2018-09-16 22:03:42 -0600

    Fix typo of notNullablue

--- db/migrations/20180916215236_initial.js
+++ db/migrations/20180916215236_initial.js
@@ -2,7 +2,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.createTable('users', table => {
-      table.uuid('id').notNullablue().primary();
+      table.uuid('id').notNullable().primary();
       table.string('username').unique();
       table.string('password');
       table.timestamps(true, true)
