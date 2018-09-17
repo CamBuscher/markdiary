fab7658da950bb2247b1ebaa80fe14be1fac7a5b
Cameron Buscher <cameronbuscher@gmail.com>
2018-09-17 17:55:36 -0600

    Add get request for users

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration)
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
+app.get('/api/v1/users', (req, res) => {
+  database('users').select()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(error => {
+      res.status(500).json({error});
+    })
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
